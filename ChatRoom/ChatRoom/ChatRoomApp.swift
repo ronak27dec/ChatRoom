1640260ca494901f26de690b94b02edc71c9a8fd
Ronak Mallick <rmal0008@synchronoss.com>
2022-01-13 23:36:32 +0530

    Initial commit

---
+++ ChatRoom/ChatRoom/ChatRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatRoomApp.swift
+//  ChatRoom
+//
+//  Created by Ronak on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
